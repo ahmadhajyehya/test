6be11c226b3c253bc0e66ccf285fae116d59b71f
dgotlieb <Dgotlieb8@gmail.com>
2023-01-27 15:22:53 +0200

    Create class_test.py

---
+++ Class5/class_test.py
@@ -0,0 +1,14 @@
+class Shark:
+    def swim(self):
+        print("The shark is swimming")
+
+    def eat(self):
+        print("The shark is eating")
+
+
+def main():
+    sammy = Shark()
+    sammy.swim()
+    sammy.eat()
+
+main()
